decfbf419347625a53b7b7bb7c30f450ffbd7e77
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-05-31 16:39:32 -0500

    feat: add react project with typescript and test modules

---
+++ src/pages/Login.test.tsx
@@ -0,0 +1,143 @@
+import * as ReactDOM from 'react-dom';
+import Login from '../../src/pages/Login';
+import { fireEvent, waitFor } from '@testing-library/react';
+import { User } from '../../src/models/User';
+
+const someUser: User = {
+    name: 'someUser',
+    email: 'someEmail'
+};
+
+describe('Login component test suite', () => {
+
+    let container: HTMLDivElement;
+    const authServiceMock = {
+        login: jest.fn()
+    };
+    const setUserMock = jest.fn();
+   //  const historyMock = history;
+
+   //  history.push = jest.fn();
+
+    beforeEach(()=> {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(<Login />, container);
+    });
+
+    test('Render correctly', () => {
+        const title = container.querySelector('h2');
+        expect(title!.textContent).toBe('Login to spaces');
+
+        const inputs = container.querySelectorAll('input');
+        expect(inputs).toHaveLength(2);
+        expect(inputs[0].value).toBe('');
+        expect(inputs[1].value).toBe('');
+
+        const submitButton = container.querySelectorAll('button');
+        expect(submitButton).toHaveLength(1);
+        expect(submitButton[0].type).toBe('submit');
+
+        const span = container.querySelector('div.login-message');
+        expect(span).not.toBeInTheDocument();
+    });
+
+    test('Passes credentials correctly', async() => {
+        const inputs = container.querySelectorAll('input');
+        const submitButton = container.querySelectorAll('button');
+
+        const username = inputs[0];
+        const password = inputs[1];
+
+        fireEvent.change(username, {
+            target: {
+                value: 'someUser'
+            }
+        });
+
+        fireEvent.change(password, {
+            target: {
+                value: 'somePassword'
+            }   
+        });
+
+        fireEvent.submit(submitButton[0]);
+
+        expect(authServiceMock.login).toBeCalledWith(
+            'someUser',
+            'somePassword'
+        );
+    });
+
+    test('Correctly handles login success', async () => {
+        authServiceMock.login.mockResolvedValueOnce(someUser);
+        const inputs = container.querySelectorAll('input');
+        const submitButton = container.querySelectorAll('button');
+
+        const username = inputs[0];
+        const password = inputs[1];
+
+        fireEvent.change(username, {
+            target: {
+                value: 'someUser'
+            }
+        });
+
+        fireEvent.change(password, {
+            target: {
+                value: 'somePassword'
+            }   
+        });
+
+        fireEvent.submit(submitButton[0]);
+        
+        await waitFor(() => expect(container.querySelectorAll('div.login-message')).toHaveLength(1));
+        await waitFor(() => expect(container.querySelector('div.login-message span')).toHaveTextContent('Login successful'));
+        expect(setUserMock).toBeCalledWith(someUser);
+      //   expect(historyMock.push).toBeCalledWith('/profile');
+    });
+
+
+    test('Correctly handles login failure', async () => {
+        authServiceMock.login.mockResolvedValueOnce(undefined);
+        const inputs = container.querySelectorAll('input');
+        const submitButton = container.querySelectorAll('button');
+
+        const username = inputs[0];
+        const password = inputs[1];
+
+        fireEvent.change(username, {
+            target: {
+                value: 'someUser'
+            }
+        });
+
+        fireEvent.change(password, {
+            target: {
+                value: 'somePassword'
+            }   
+        });
+
+        fireEvent.submit(submitButton[0]);
+        
+        const status = await waitFor(() => {
+            const message = container.querySelector('div.login-message');
+            if (message?.children.length == 1) {
+                return {
+                    node: message || null,
+                    message: message?.querySelector('span'),
+                }
+            }
+            return null;
+        });
+
+        expect(status?.node).toBeInTheDocument();
+        expect(status?.message).toHaveTextContent('Login failed');
+    });
+
+    afterEach(() => {
+        document.body.removeChild(container);
+        container.remove();
+        jest.clearAllMocks();
+    });
+});
---
+++ src/pages/Login.tsx
@@ -0,0 +1,107 @@
+import React, { ChangeEvent, Component, FormEvent } from 'react'
+
+interface State {
+  username: string,
+  password: string,
+  loginAttempted: boolean,
+  loginSuccessfull: boolean
+}
+
+class Login extends Component<{}, State> {
+
+  state: State = {
+    username: '',
+    password: '',
+    loginAttempted: false,
+    loginSuccessfull: false
+  }
+
+  private onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  private onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  private onLoginEvent = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.setState({
+      loginAttempted: true
+    });
+    // const res = await this.props
+    //   .authService
+    //   .login(this.state.username, this.state.password);
+    
+    //   if (res) {
+    //     this.setState({
+    //       loginSuccessfull: true
+    //     });
+    //     this.props.setUser(res);
+    //     history.push('/profile');
+    //   } else {
+    //     this.setState({
+    //       loginSuccessfull: false
+    //     });
+    //   }
+  }
+
+  render() {
+    const {
+      username,
+      password,
+      loginAttempted,
+      loginSuccessfull
+    } = this.state;
+
+    let loginMessage: any;
+
+    if(loginAttempted) {
+      if (loginSuccessfull) {
+        loginMessage = <div className='login-message'>
+            <span>Login successful</span>
+          </div>;
+      } else {
+        loginMessage = <div className='login-message'>
+          <span>Login failed</span>
+        </div>;
+      }
+    }
+    return (
+      <div>
+        <h2>Login to spaces</h2>
+        <form
+          onSubmit={this.onLoginEvent}
+        >
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={this.onChangeUsername}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={this.onChangePassword}
+          />
+          <button
+            type='submit'
+          >Login</button>
+          {
+            loginMessage
+          }
+        </form>
+
+      </div>
+    )
+  }
+}
+
+export default Login;
